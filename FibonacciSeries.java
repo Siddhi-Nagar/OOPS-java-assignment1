8bf5883ca89c6a095ef665d29dc33312ddfa7049
Siddhi-Nagar <nagarsiddhi11@gmail.com>
2024-11-24 15:10:11 +0530

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms in the Fibonacci series: ");
+        int terms = input.nextInt();
+
+        int first = 0, second = 1;
+
+        System.out.println("Fibonacci Series up to " + terms + " terms:");
+
+        if (terms >= 1) {
+            System.out.print(first + " ");
+        }
+        if (terms >= 2) {
+            System.out.print(second + " ");
+        }
+
+        for (int i = 3; i <= terms; i++) {
+            int next = first + second;
+            System.out.print(next + " ");
+            first = second;
+            second = next;
+        }
+        input.close();
+    }
+}
