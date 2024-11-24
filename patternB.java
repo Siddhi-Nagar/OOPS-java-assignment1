c49f86eb01c178ddde6230e04f2805cb2db282d8
Siddhi-Nagar <nagarsiddhi11@gmail.com>
2024-11-24 16:32:13 +0530

    Create patternB.java

---
+++ patternB.java
@@ -0,0 +1,16 @@
+public class patternB {
+    public static void main(String[] args) {
+        int n = 4; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((i - j + 1) + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
