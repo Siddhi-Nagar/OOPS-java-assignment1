c3daf11c3e73ad0d5b8350653307c3f3ecb86c5c
Siddhi-Nagar <nagarsiddhi11@gmail.com>
2024-11-24 18:20:25 +0530

    Create zeroArgumentConstructor.java

---
+++ zeroArgumentConstructor.java
@@ -0,0 +1,10 @@
+public class zeroArgumentConstructor {
+     public zeroArgumentConstructor() {
+            System.out.println("This is a 0-arguments constructor.");
+        }
+    
+        public static void main(String[] args){
+            zeroArgumentConstructor obj = new zeroArgumentConstructor();
+        }
+    }
+    
